f818dbf4afa670e20169764111453978697ddc0d
InakiMedina <inaki.medina@gmail.com>
2026-05-10 14:33:27 -0600

    imports into ecrs to not duplicate ecr

--- terraform/ecrs.tf
+++ terraform/ecrs.tf
@@ -1,3 +1,18 @@
+import {
+  to = aws_ecr_repository.catalog
+  id = "catalog-service"
+}
+
+import {
+  to = aws_ecr_repository.sales
+  id = "sales-service"
+}
+
+import {
+  to = aws_ecr_repository.notification
+  id = "notification-service"
+}
+
 # Catalog ECR
 resource "aws_ecr_repository" "catalog" {
   name = "catalog-service"
