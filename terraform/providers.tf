fe72bb6d1925e5b4212e56b53553f8ee9cdeef89
InakiMedina <inaki.medina@gmail.com>
2026-05-10 14:11:59 -0600

    fixed the working directory workflow

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,4 +4,26 @@ terraform {
     key            = "core/terraform.tfstate"
     region         = "us-east-1"
   }
+
+  
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# 3. THE SETTINGS: Configures the specific details for those providers
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "github" {
+  token = var.github_token # Uses the GH_PAT secret you just set up
+  owner = "CloudExam2"  # Ensures it builds secrets in your organization
 }
