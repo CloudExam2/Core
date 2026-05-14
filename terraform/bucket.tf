4d11a9c5caca427a9a16a3a213bd42c2b5a06bac
InakiMedina <inaki.medina@gmail.com>
2026-05-14 11:28:02 -0600

    randomizes the notification bucket name so its unique

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,8 +1,17 @@
-# S3 Bucket for Tickets
-resource "aws_s3_bucket" "tickets" {
-  bucket = "iteso-tickets-377871695195" # Must be globally unique
+# Generates a random suffix
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
+# S3 Bucket with dynamic name
+resource "aws_s3_bucket" "tickets" {
+  # This results in something like: iteso-tickets-a1b2c3d4
+  bucket = "iteso-tickets-${lower(random_id.bucket_suffix.hex)}"
+  
+  force_destroy = true # Allows deleting the bucket even if it has files
+}
+
+# Saves the dynamic name to GitHub so other repos can read it
 resource "github_actions_organization_variable" "bucket_name" {
   variable_name = "TICKETS_BUCKET_NAME"
   visibility    = "all"
