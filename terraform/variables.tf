fdbb6e6f481b556280d34d8c2c862aa9b7ebeb0d
InakiMedina <inaki.medina@gmail.com>
2026-05-17 18:11:07 -0600

    added sales to the vpc and modified the ci/cd to get the sales url backend variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,9 @@ variable "catalog_backend_url" {
   description = "Catalog on EC2, e.g. http://1.2.3.4:80 (no trailing slash). Leave empty on first apply; set after Catalog EC2 exists, then re-apply Core."
   default     = ""
 }
+
+variable "sales_backend_url" {
+  type        = string
+  description = "Sales on EC2, e.g. http://1.2.3.4:80 (no trailing slash). Leave empty on first apply; set after Sales EC2 exists, then re-apply Core."
+  default     = ""
+}
